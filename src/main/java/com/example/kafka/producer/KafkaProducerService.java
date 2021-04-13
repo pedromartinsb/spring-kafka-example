bdeea2efebbd87e55566068a0496e9db097b4446
Pedro Campos <pedro.campos@startaideia.com.br>
2021-04-13 16:35:22 -0300

    first commit

---
+++ src/main/java/com/example/kafka/producer/KafkaProducerService.java
@@ -0,0 +1,72 @@
+package com.example.kafka.producer;
+
+import com.example.kafka.model.Greeting;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class KafkaProducerService {
+
+    @Value(value = "${message.topic.name}")
+    private String topicName;
+
+    @Value(value = "${partitioned.topic.name}")
+    private String partitionedTopicName;
+
+    @Value(value = "${filtered.topic.name}")
+    private String filteredTopicName;
+
+    @Value(value = "${greeting.topic.name}")
+    private String greetingTopicName;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, Greeting> greetingKafkaTemplate;
+
+    public void sendMessage(String msg) {
+        this.kafkaTemplate.send(topicName, msg);
+    }
+
+    public void sendMessageListenableFuture(String message) {
+
+        ListenableFuture<SendResult<String, String>> future = this.kafkaTemplate.send(topicName, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println(
+                        "Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+
+        });
+
+    }
+
+    public void sendMessageToPartition(String message, int partition) {
+        kafkaTemplate.send(partitionedTopicName, partition, null, message);
+    }
+
+    public void sendMessageToFiltered(String message) {
+        kafkaTemplate.send(filteredTopicName, message);
+    }
+
+    // Producing Custom Messages
+    public void sendGreetingMessage(Greeting greeting) {
+        this.greetingKafkaTemplate.send(greetingTopicName, greeting);
+    }
+
+}
